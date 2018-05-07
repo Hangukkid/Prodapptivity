156022318068ffae7440fab3395244b702e04bc4
PuaChris <chris.pua@mail.utoronto.ca>
2018-05-07 11:44:31 -0400

    Modified timer so user can start, pause, resume, and stop the timer. Bugs still need to be fixed when pressing pause, and then start again.

--- app/src/main/java/matthew/won/utoronto/prod/Main_Screen.java
+++ app/src/main/java/matthew/won/utoronto/prod/Main_Screen.java
@@ -10,41 +10,156 @@ import android.os.CountDownTimer;
 
 public class Main_Screen extends AppCompatActivity {
 
-    public long startTime = 10000;
-    public long timeIntervals = 250;
-    public long timeLeft;
+    private long timeRemaining = 0;
+
+    private boolean isPaused = false;
+    private boolean isCancelled = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        //Super allows us to run existing code from an inherited class on top of the code we are going to write
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main__screen);
 
-        final TextView timerValue = (TextView) findViewById(R.id.timerValue);
-        final Button countDownBtn = (Button) findViewById(R.id.countDownBtn);
 
-        countDownBtn.setOnClickListener(new View.OnClickListener() {
+        //Final means its value will never be changed
+        final TextView timerValue = (TextView) findViewById(R.id.timerValue);
+        final Button startBtn = (Button) findViewById(R.id.startBtn);
+        final Button pauseBtn = (Button) findViewById(R.id.pauseBtn);
+        final Button resumeBtn = (Button) findViewById(R.id.resumeBtn);
+        final Button stopBtn = (Button) findViewById(R.id.stopBtn);
+
+
+        //Change to a lighter grey to indicate it cannot be clicked
+        pauseBtn.setEnabled(false);
+        resumeBtn.setEnabled(false);
+        stopBtn.setEnabled(false);
+
+        startBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                isPaused = false;
+                isCancelled = false;
 
-                CountDownTimer timer = new CountDownTimer(startTime, timeIntervals) {
+                pauseBtn.setEnabled(true);
+                stopBtn.setEnabled(true);
+
+                startBtn.setEnabled(false);
+                resumeBtn.setEnabled(false);
+
+
+                //30 seconds
+                final long millisInFuture = 30000;
+
+                //1 second
+                long countDownInterval = 1000;
+
+                //Initialize a new CountDownTimer instance
+                CountDownTimer timer;
+                timer = new CountDownTimer(millisInFuture, countDownInterval) {
                     @Override
                     public void onTick(long millisUntilFinished) {
-                        timeLeft = millisUntilFinished/1000;
-                        timerValue.setText(timeLeft + "");
-
+                        if (isPaused || isCancelled){
+                            cancel();
+                        }
+                        else {
+                            timerValue.setText(millisUntilFinished/1000 + "");
+                            timeRemaining = millisUntilFinished;
+                        }
                     }
 
                     @Override
                     public void onFinish() {
-                        timerValue.setText("Finished");
+                        timerValue.setText("Done");
+
+                        startBtn.setEnabled(true);
+
+                        resumeBtn.setEnabled(false);
+                        pauseBtn.setEnabled(false);
+                        stopBtn.setEnabled(false);
+
                     }
-
-
-                };
-
-                timer.start();
+                }.start();
             }
         });
+
+        pauseBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isPaused = true;
+                isCancelled = false;
+
+                startBtn.setEnabled(true);
+                resumeBtn.setEnabled(true);
+                stopBtn.setEnabled(true);
+
+                pauseBtn.setEnabled(false);
+
+
+            }
+        });
+
+        resumeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isPaused = false;
+                isCancelled = false;
+
+                pauseBtn.setEnabled(true);
+                stopBtn.setEnabled(true);
+
+                startBtn.setEnabled(false);
+                resumeBtn.setEnabled(false);
+
+                final long millisInFuture = timeRemaining;
+                long countDownInterval = 1000;
+
+                CountDownTimer timer;
+                timer = new CountDownTimer(millisInFuture, countDownInterval) {
+                    @Override
+                    public void onTick(long millisUntilFinished) {
+                        if (isPaused || isCancelled){
+                            cancel();
+                        }
+                        else {
+                            timerValue.setText(millisUntilFinished/1000 + "");
+                            timeRemaining = millisUntilFinished;
+                        }
+                    }
+
+                    @Override
+                    public void onFinish() {
+                        timerValue.setText("Done");
+
+                        startBtn.setEnabled(true);
+
+                        resumeBtn.setEnabled(false);
+                        pauseBtn.setEnabled(false);
+                        stopBtn.setEnabled(false);
+                    }
+                }.start();
+
+                stopBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        isCancelled = true;
+
+                        startBtn.setEnabled(true);
+
+                        pauseBtn.setEnabled(false);
+                        resumeBtn.setEnabled(false);
+                        stopBtn.setEnabled(false);
+
+                        timerValue.setText("Timer stopped");
+                    }
+                });
+
+
+            }
+        });
+
+
 
     }
 }
