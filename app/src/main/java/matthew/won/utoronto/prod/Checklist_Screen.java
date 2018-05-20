ea766bd12002d7053a789f85f0322cbbeb887761
PuaChris <chris.pua@mail.utoronto.ca>
2018-05-20 12:51:31 -0400

    Added database and can add tasks (but won't be saved onto screen yet)

--- app/src/main/java/matthew/won/utoronto/prod/Checklist_Screen.java
+++ app/src/main/java/matthew/won/utoronto/prod/Checklist_Screen.java
@@ -16,13 +16,17 @@ import java.util.ArrayList;
 //Website to create the list:
 //https://guides.codepath.com/android/Basic-Todo-App-Tutorial
 
+/*
+TODO: find out how to add items onto a listView without having to create an array everytime the activity starts
+ */
 
 public class Checklist_Screen extends AppCompatActivity {
 
     private Toolbar toolbar;
 
+    TaskDatabaseHelper taskDBHelper;
 
-    private ArrayList<String> tasks;
+    private ArrayList<String> checklist;
     private ArrayAdapter<String> task_adapter;
     private ListView checklist_view;
     private EditText new_task_text;
@@ -38,46 +42,43 @@ public class Checklist_Screen extends AppCompatActivity {
 
         android.support.v7.app.ActionBar actionbar = getSupportActionBar();
         actionbar.setDisplayHomeAsUpEnabled(true);
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
 
         checklist_view = (ListView) findViewById(R.id.checklist_view);
-        tasks = new ArrayList<String>();
+        new_task_text = (EditText) findViewById(R.id.new_task_text);
+
+        taskDBHelper = new TaskDatabaseHelper(this, null, null, 1);
+
+        checklist = new ArrayList<String>();
 
         //Need to add own "TextView" resource, not activity containing TextView
-        task_adapter = new ArrayAdapter<String>(this, R.layout.checklist_item, tasks);
+        task_adapter = new ArrayAdapter<String>(this, R.layout.checklist_item, checklist);
         checklist_view.setAdapter(task_adapter);
 
-        setupListViewListener();
-
-        //Used to prevent the keyboard from appearing when starting activity
-        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
     }
 
-    public void addTask(View v) {
-        new_task_text = (EditText) findViewById(R.id.new_task_text);
-        String new_task_name = new_task_text.getText().toString();
-        if (new_task_name != null && !new_task_name.isEmpty()) {
-            task_adapter.add(new_task_name);
+    public void addTaskOnClick(View view) {
+        String new_task_string = new_task_text.getText().toString();
+        addTask(new_task_string);
+    }
+
+
+    public void addTask(String new_task_string) {
+        if (new_task_string != null && !new_task_string.isEmpty()) {
+            Tasks task = new Tasks(new_task_string);
+            taskDBHelper.addTask(task);
             new_task_text.setText("");
 
+            addTaskFromDatabase();
         }
     }
 
-    public void setupListViewListener() {
-        checklist_view.setOnItemLongClickListener(
-                new AdapterView.OnItemLongClickListener() {
-                    @Override
-                    public boolean onItemLongClick(AdapterView<?> adapter,
-                                                   View item, int pos, long id) {
-                        // Remove the item within array at position
-                        tasks.remove(pos);
-                        // Refresh the adapter
-                        task_adapter.notifyDataSetChanged();
-                        // Return true consumes the long click event (marks it handled)
-                        return true;
-                    }
-
-                });
+    public void addTaskFromDatabase(){
+        String new_task_to_array = taskDBHelper.mostRecentTaskToString();
+        task_adapter.add(new_task_to_array);
     }
+
+
 
 }
 
---
+++ app/src/main/java/matthew/won/utoronto/prod/TaskDatabaseHelper.java
@@ -0,0 +1,91 @@
+package matthew.won.utoronto.prod;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.Cursor;
+import android.content.Context;
+import android.content.ContentValues;
+
+public class TaskDatabaseHelper extends SQLiteOpenHelper{
+    private static final int DATABASE_VERSION = 1;
+
+    private static final String DATABASE_NAME  = "tasks.db";
+    public static final String TABLE_TASKS = "tasks";
+    public static final String COLUMN_ID = "_id";
+    public static final String COLUMN_TASKNAME = "taskname";
+
+    public TaskDatabaseHelper (Context context, String name, SQLiteDatabase.CursorFactory factory, int version){
+        super (context, DATABASE_NAME, factory, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate (SQLiteDatabase task_db){
+        String query = "CREATE TABLE " + TABLE_TASKS + "(" +
+                COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COLUMN_TASKNAME + " TEXT " +
+                ");";
+
+        task_db.execSQL(query);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase task_db, int oldVersion, int newVersion){
+        task_db.execSQL("DROP TABLE IF EXISTS " + TABLE_TASKS);
+        onCreate(task_db);
+    }
+
+    public void addTask(Tasks task){
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_TASKNAME, task.getTask_name());
+
+        SQLiteDatabase task_db = getWritableDatabase();
+        task_db.insert(TABLE_TASKS, null, values);
+
+        task_db.close();
+    }
+
+    public void deleteTask (String task_name){
+        SQLiteDatabase task_db = getWritableDatabase();
+        task_db.execSQL("DELETE FROM " + TABLE_TASKS + " WHERE " + COLUMN_TASKNAME + "=\"" + task_name + "\";");
+    }
+
+    public String mostRecentTaskToString(){
+        String task_string = "";
+        SQLiteDatabase task_db = getWritableDatabase();
+        String query = "SELECT * FROM " + TABLE_TASKS + " WHERE 1";
+
+        Cursor c = task_db.rawQuery(query, null);
+
+
+        c.moveToLast();
+        if (c.getString(c.getColumnIndex("taskname"))!= null){
+            task_string += c.getString(c.getColumnIndex("taskname"));
+        }
+
+        task_db.close();
+        return task_string;
+    }
+
+    public String taskDatabaseToString(){
+        String task_string = "";
+        SQLiteDatabase task_db = getWritableDatabase();
+        String query = "SELECT * FROM " + TABLE_TASKS + " WHERE 1";
+
+        Cursor c = task_db.rawQuery(query, null);
+
+        c.moveToFirst();
+
+        while (!c.isAfterLast()){
+            if (c.getString(c.getColumnIndex("taskname"))!= null){
+                task_string += c.getString(c.getColumnIndex("taskname"));
+
+                task_string += "\n";
+            }
+            c.moveToNext();
+        }
+        task_db.close();
+        return task_string;
+    }
+
+
+}
